a23d2b9071b3994a23b4272f7daa86de33351960
RKnagre <nagrek205@gmail.com>
2023-05-06 02:15:21 +0530

    codepush

---
+++ src/main/java/com/example/yantra/YantraApplication.java
@@ -0,0 +1,13 @@
+package com.example.yantra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YantraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YantraApplication.class, args);
+	}
+
+}
